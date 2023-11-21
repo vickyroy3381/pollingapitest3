d4c3ba197cbe12aa80e27f2dbe5782b46e8cebef
vickyroy3381 <128716321+vickyroy3381@users.noreply.github.com>
2023-11-21 17:33:19 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,15 +7,14 @@ const mongodbUrl = process.env.MONGODB_URL;
 //connect to the database
 mongoose.connect(mongodbUrl);
 
-//acquire the connection(to check if it's successful)
 const db = mongoose.connection;
 
-//error
+//error function
 db.on("error", function (err) {
   console.log(err.message);
 });
 
-//up and running then print the message
+//if database is connected successfully,then print the message
 db.once("open", function () {
   console.log("Successfully connected to the database");
 });
